c16848aaa059381797e20b239b6d9976e06c9ef3
David Yarde <davidyarde@gmail.com>
2016-01-29 12:40:02 -0500

    - assign role based on woocommerce form

--- howl-content/themes/listify/functions.php
+++ howl-content/themes/listify/functions.php
@@ -579,7 +579,7 @@ foreach ( $integrations as $file => $dependancy ) {
 
 
 /**
- * Add new register fields for WooCommerce registration.
+ * Add new register fields for customer WooCommerce registration.
  *
  * @return string Register fields HTML.
  */
@@ -604,35 +604,8 @@ function wooc_extra_register_fields() {
 <?php
 add_action( 'woocommerce_register_form_start', 'wooc_extra_register_fields' );
 
-function howl_pro_form() {?> 
-	<?php if ( is_page('pro-sign-up') ) : ?>
-		<p class="form-row form-row-wide">
-			<label for="reg_business_service"><?php _e( 'What kind of services do you offer?', 'woocommerce' ); ?></label>
-  		<input type="text" class="input-text" name="business_primary_service" id="reg_business_service" value="<?php if ( ! empty( $_POST['business_primary_service'] ) ) esc_attr_e( $_POST['business_primary_service'] ); ?>" />
-		</p>
-
-		<p class="form-row form-row-wide">
-			<label for="reg_business_location"><?php _e( 'Where is your business located?', 'woocommerce' ); ?></label>
-  		<input type="text" class="input-text" name="business_location" id="reg_business_location" value="<?php if ( ! empty( $_POST['business_location'] ) ) esc_attr_e( $_POST['business_location'] ); ?>" />
-		</p>
-
-	<?php  
-		// wp_update_user(array(
-		//   'ID' => $customer_id,
-		//   'role' => 'professional' // Update to desired role
-		// ));	
-	?>		
-	<?php endif;
-}
-?>
-<?php
-
-add_action( 'register_form', 'howl_pro_form' );
-
-
-
 /**
- * Validate the extra register fields.
+ * Validate the extra customer register fields.
  *
  * @param  string $username          Current username.
  * @param  string $email             Current email.
@@ -654,7 +627,7 @@ function wooc_validate_extra_register_fields( $username, $email, $validation_err
 add_action( 'woocommerce_register_post', 'wooc_validate_extra_register_fields', 10, 3 );
 
 /**
- * Save the extra register fields.
+ * Save the extra customer register fields.
  *
  * @param  int  $customer_id Current customer ID.
  *
@@ -679,6 +652,76 @@ function wooc_save_extra_register_fields( $customer_id ) {
 }
 
 add_action( 'woocommerce_created_customer', 'wooc_save_extra_register_fields' );
+
+// Extra fields for professional sign up form short flow
+function howl_pro_form() {?> 
+	<?php if ( is_page('pro-sign-up') ) : ?>
+		<p class="form-row form-row-wide">
+			<label for="reg_business_service"><?php _e( 'What kind of services do you offer?', 'woocommerce' ); ?></label>
+  		<input type="text" class="input-text" name="business_primary_service" id="reg_business_service" value="<?php if ( ! empty( $_POST['business_primary_service'] ) ) esc_attr_e( $_POST['business_primary_service'] ); ?>" />
+		</p>
+
+		<p class="form-row form-row-wide">
+			<label for="reg_business_location"><?php _e( 'Where is your business located?', 'woocommerce' ); ?></label>
+  		<input type="text" class="input-text" name="business_location" id="reg_business_location" value="<?php if ( ! empty( $_POST['business_location'] ) ) esc_attr_e( $_POST['business_location'] ); ?>" />
+		</p>
+		
+	<?php endif;
+}
+?>
+<?php
+
+add_action( 'register_form', 'howl_pro_form' );
+
+/**
+ * Validate the extra professional register fields.
+ *
+ * @param  string $username          Current username.
+ * @param  string $email             Current email.
+ * @param  object $validation_errors WP_Error object.
+ *
+ * @return void
+ */
+function wooc_validate_howl_pro_form( $username, $email, $validation_errors ) {
+  if ( isset( $_POST['business_primary_service'] ) && empty( $_POST['business_primary_service'] ) ) {
+    $validation_errors->add( 'billing_business_primary_service_error', __( '<strong>Error</strong>: Primary business service is required!', 'woocommerce' ) );
+  }
+
+  if ( isset( $_POST['business_location'] ) && empty( $_POST['business_location'] ) ) {
+    $validation_errors->add( 'billing_business_location_error', __( '<strong>Error</strong>: Business location is required!.', 'woocommerce' ) );
+  }
+
+}
+
+add_action( 'woocommerce_register_post', 'wooc_validate_howl_pro_form', 10, 3 );
+
+/**
+ * Save the extra professional register fields.
+ *
+ * @param  int  $customer_id Current customer ID.
+ *
+ * @return void
+ */
+function wooc_save_extra_howl_pro_form_fields( $customer_id ) {
+	if ( isset( $_POST['business_primary_service'] ) ) {
+		// WordPress default first name field.
+		update_user_meta( $customer_id, 'business_primary_service', sanitize_text_field( $_POST['business_primary_service'] ) );
+	
+		wp_update_user(array(
+		  'ID' => $customer_id,
+		  'role' => 'professional' // Update to desired role
+		));	
+
+	}
+
+	if ( isset( $_POST['business_location'] ) ) {
+		// WordPress default last name field.
+		update_user_meta( $customer_id, 'business_location', sanitize_text_field( $_POST['business_location'] ) );
+	}
+	
+}
+
+add_action( 'woocommerce_created_customer', 'wooc_save_extra_howl_pro_form_fields' );
 
 /**
  * Redirect users to custom URL based on their role after login
