9560c88954adde496c6b6ec57eeda67060ffd1da
David Yarde <davidyarde@gmail.com>
2016-02-29 00:37:46 -0500

    - edit project flow fixes

--- howl-content/themes/listify/single-customer_projects.php
+++ howl-content/themes/listify/single-customer_projects.php
@@ -7,20 +7,32 @@
 
 get_header(); ?>
 
+
+<div id="primary" class="container">
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php $author = get_the_author(); ?>
+	
+		<div class="row content-area">
 
+			<main id="main" class="site-main col-md-8 col-sm-7 col-xs-12" role="main">
+				<?php get_template_part( 'content', 'customer_projects' ); ?>
+			</main>
 
+    <?php  
+    $edit_post_data      = isset($_GET['_post']) ? $_GET['_post'] : null;
+    $edit_post_id        = isset($edit_post_data['ID']) ? absint($edit_post_data['ID']) : -1;
+    $edit_post_status    = get_post_status($edit_post_id);
+    $edit_post_published = ( 'ongoing' === $edit_post_status ) ? true : false;
 
-	<div id="primary" class="container">
-			<div class="row content-area">
+    // Hide field if editing a published post
+    if ( ! $edit_post_published ) {
+        // My piklist field(s)
 
-				<main id="main" class="site-main col-md-8 col-sm-7 col-xs-12" role="main">
-					<?php get_template_part( 'content', 'customer_projects' ); ?>
-				</main>
-
+    }
+    ?>
+    </div>
 	<?php endwhile; ?>
-<!-- 			</div>
-	</div> -->
-</div>
+
+
+  </div>
 <?php get_footer(); ?>
