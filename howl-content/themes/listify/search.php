f77a697682d15b10ff4854fab0c9379eb864d4b3
Antonio Fernandes <afj176@gmail.com>
2016-03-27 11:52:46 -0400

    mapped search to blog styles

--- howl-content/themes/listify/search.php
+++ howl-content/themes/listify/search.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 					<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php get_template_part( 'content' ); ?>
+						<?php get_template_part( 'content', 'blog' ); ?>
 
 					<?php endwhile; ?>
 
